3a8b9e7d9647e083e71a4a850defaf114ed7e5fd
jenjnif <jenjnif@hotmail.com>
2018-12-10 06:59:40 +0000

    basic code working

--- counting valleys/counting_valleys.py
+++ counting valleys/counting_valleys.py
@@ -1,23 +1,42 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
+
 
 # Complete the countingValleys function below.
 def countingValleys(n, s):
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # if __name__ == '__main__':
+    #     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input())
+    #     n = int(input())
+    #     s = input()
+    level = 0
+    valleys = 0
+    first_time_valley = None
+    for step in s:
+        if 'D' in step:
+            level -= 1
+            print(level)
+            if level == -2:
+                first_time_valley = True
+        elif 'U' in step:
+            level += 1
+            print(level)
+            if first_time_valley is True:
+                if level == 0:
+                    first_time_valley = False
+                    valleys += 1
 
-    s = input()
+    print("You have walked through " + str(valleys) + " valleys.")
 
-    result = countingValleys(n, s)
 
-    fptr.write(str(result) + '\n')
+# result = countingValleys(n, s)
+# fptr.write(str(result) + '\n')
 
-    fptr.close()
+    #     # fptr.close()
+    # print(level)
+
+
+countingValleys(8, ['D', 'D', 'D', 'U', 'U', 'U'])
+# countingValleys(8, ['U', 'D', 'D', 'D', 'U', 'D', 'U', 'U'])
