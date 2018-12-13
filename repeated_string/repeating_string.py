97c13818b23139f631aa71b0985a38c75d015621
jenjnif <jenjnif@hotmail.com>
2018-12-13 21:19:24 +0000

    code totalling a's in full strings

--- repeated_string/repeating_string.py
+++ repeated_string/repeating_string.py
@@ -9,24 +9,32 @@
 # Complete the repeatedString function below.
 def repeatedString(s, n):
 
-    infinite_string = ''
+    leftover = n % len(s)
+    print(leftover)
+    times = n // len(s)
+    print(times)
     a_counter = 0
 
-    while len(infinite_string) < n:
-        for letter in s:
-            if len(infinite_string) <= n:
-                infinite_string += letter
-    for letter in infinite_string:
+    for letter in s:
         if 'a' in letter:
             a_counter += 1
+            print(a_counter)
 
-    print(a_counter)
-    return a_counter
+    # for letter in s[0:0]:
+    #     print('leftover')
+    #     print(len(s))
+    #     if 'a' in letter:
+    #         a_counter += 1
+    #         print(a_counter)
+
+    total_string = a_counter * times
+
+    print(total_string)
 
 # 7
-# repeatedString('aba', 10)
-# 1000000000000
-repeatedString('a', 1000000000000)
+repeatedString('aba', 10)
+# # 1000000000000
+# repeatedString('a', 1000000000000)
 
 # if __name__ == '__main__':
 #     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -40,3 +48,17 @@ repeatedString('a', 1000000000000)
 #     fptr.write(str(result) + '\n')
 
 #     fptr.close()
+
+    # infinite_string = ''
+    # a_counter = 0
+
+    # while len(infinite_string) < n:
+    #     for letter in s:
+    #         if len(infinite_string) <= n:
+    #             infinite_string += letter
+    # for letter in infinite_string:
+    #     if 'a' in letter:
+    #         a_counter += 1
+
+    # print(a_counter)
+    # return a_counter
