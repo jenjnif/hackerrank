66e1e5d578bd2da1c38b77bd2559377798ad34a3
jenjnif <jenjnif@hotmail.com>
2018-12-13 21:41:13 +0000

    get unrefined version working

--- repeated_string/repeating_string.py
+++ repeated_string/repeating_string.py
@@ -9,32 +9,33 @@
 # Complete the repeatedString function below.
 def repeatedString(s, n):
 
-    leftover = n % len(s)
-    print(leftover)
-    times = n // len(s)
-    print(times)
+    number_leftover = n % len(s)
+    print(number_leftover)
+    whole_times_repeated = n // len(s)
+    print(whole_times_repeated)
+    string_left = s[0:number_leftover]
+    print(string_left)
     a_counter = 0
+    leftover_counter = 0
 
     for letter in s:
         if 'a' in letter:
             a_counter += 1
             print(a_counter)
 
-    # for letter in s[0:0]:
-    #     print('leftover')
-    #     print(len(s))
-    #     if 'a' in letter:
-    #         a_counter += 1
-    #         print(a_counter)
+    for letter in string_left:
+        if 'a' in letter:
+            leftover_counter += 1
+            print('here is ' + str(leftover_counter))
 
-    total_string = a_counter * times
+    total_string = a_counter * whole_times_repeated + leftover_counter
 
     print(total_string)
 
 # 7
-repeatedString('aba', 10)
+# repeatedString('aba', 10)
 # # 1000000000000
-# repeatedString('a', 1000000000000)
+repeatedString('a', 1000000000000)
 
 # if __name__ == '__main__':
 #     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -48,17 +49,3 @@ repeatedString('aba', 10)
 #     fptr.write(str(result) + '\n')
 
 #     fptr.close()
-
-    # infinite_string = ''
-    # a_counter = 0
-
-    # while len(infinite_string) < n:
-    #     for letter in s:
-    #         if len(infinite_string) <= n:
-    #             infinite_string += letter
-    # for letter in infinite_string:
-    #     if 'a' in letter:
-    #         a_counter += 1
-
-    # print(a_counter)
-    # return a_counter
