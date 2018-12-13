839319326e02d4c5bb46f749c698db2411295447
jenjnif <jenjnif@hotmail.com>
2018-12-13 20:47:07 +0000

    building a slow way of doing it

--- repeated_string/repeating_string.py
+++ repeated_string/repeating_string.py
@@ -9,18 +9,24 @@
 # Complete the repeatedString function below.
 def repeatedString(s, n):
 
+    infinite_string = ''
     a_counter = 0
 
-    for letter in s:
-        if 'a':
+    while len(infinite_string) < n:
+        for letter in s:
+            if len(infinite_string) <= n:
+                infinite_string += letter
+    for letter in infinite_string:
+        if 'a' in letter:
             a_counter += 1
 
+    print(a_counter)
     return a_counter
 
 # 7
-# repeatedString(aba, 10)
+# repeatedString('aba', 10)
 # 1000000000000
-# repeatedString(a, 1000000000000)
+repeatedString('a', 1000000000000)
 
 # if __name__ == '__main__':
 #     fptr = open(os.environ['OUTPUT_PATH'], 'w')
