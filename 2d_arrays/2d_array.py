59255cceb84dac7f3e871d89278f6924bba8e16a
jenjnif <jenjnif@hotmail.com>
2018-12-31 18:42:37 +0000

    list top row test fail

--- 2d_arrays/2d_array.py
+++ 2d_arrays/2d_array.py
@@ -34,5 +34,9 @@ grid is:
 
 # Test that I can list all the cells in the hourglass for each example
 # position in the grid
-def test_list_hourlgass_numbers():
+def test_find_any_number():
     assert find_my_hourglass(3, 1, arr) == 4
+
+
+def test_list_top_row_hourlgass():
+    assert find_my_hourglass(0, 0, arr) == [-9, -9, -9]
