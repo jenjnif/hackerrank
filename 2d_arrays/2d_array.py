8ea00c16ae3d7ae12c25e6d54b5a00383525fa1f
jenjnif <jenjnif@hotmail.com>
2019-01-04 19:59:30 +0000

    write test to find each number in an hourglass

--- 2d_arrays/2d_array.py
+++ 2d_arrays/2d_array.py
@@ -14,10 +14,8 @@ def find_coordinates(width, index):
 
     x = index % 6
     y = index // 6
-    index += 1
-
     coordinates = (x, y)
-    print(coordinates)
+
     return coordinates
 
 
@@ -26,6 +24,10 @@ def find_my_cell(x, y, arr):
     number = arr[(y * 6) + x]
 
     return number
+
+
+def find_my_hourglass(coordinates, arr):
+    pass
 
 
 '''
@@ -58,3 +60,7 @@ def test_find_coordinates_one():
 
 def test_find_coordinates_two():
     assert find_coordinates(6, 16) == (4, 2)
+
+
+def test_find_hourglass():
+    assert find_my_hourglass((0, 0), arr) == [-9, -9, -9, -9, -9, -9, -9]
