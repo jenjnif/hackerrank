d748dbeba7efc5b9b4e9cd12654763b7bd67a89e
jenjnif <jenjnif@hotmail.com>
2019-01-22 13:56:39 +0000

    pass test to find list width

--- 2d_arrays/2d_array.py
+++ 2d_arrays/2d_array.py
@@ -3,8 +3,12 @@ arr = [-9, -9, -9, 1, 1, 1, 0, -9, 0, 4, 3, 2, -9, -9, -9, 1, 2, 3,
        0, 0, 8, 6, 6, 0, 0, 0, 0, -2, 0, 0, 0, 0, 1, 2, 4, 0]
 
 
-def find_width():
-    pass
+def find_width(arr):
+
+    total_length_list = len(arr)
+    width = total_length_list**(0.5)
+
+    return width
 
 
 def test_width():
