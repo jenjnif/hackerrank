e5f3dff9b59ef036c8caa25a9f20fa1cbac2edd8
jenjnif <jenjnif@hotmail.com>
2019-01-22 21:28:55 +0000

    pass test to find highest sum of hourglass

--- 2d_arrays/2d_array.py
+++ 2d_arrays/2d_array.py
@@ -7,7 +7,6 @@ def find_width(arr):
 
     total_length_list = len(arr)
     width = int(total_length_list**(0.5))
-
     return width
 
 
@@ -17,38 +16,52 @@ def find_coordinates(index):
     x = int(index % width)
     y = int(index // width)
     coordinates = (x, y)
-
     return coordinates
 
 
 def find_hourglass(coordinates):
 
+    width = find_width(arr)
     hourglass = []
     x = coordinates[0]
     y = coordinates[1]
 
     for i in range(3):
-        hourglass_top = arr[(y * 6) + x]
+        hourglass_top = arr[(y * width) + x]
         x += 1
         hourglass.append(hourglass_top)
 
     y += 1
     x -= 2
     for i in range(1):
-        hourglass_middle = arr[(y * 6) + x]
+        hourglass_middle = arr[(y * width) + x]
         hourglass.append(hourglass_middle)
 
     y += 1
     x -= 1
     for i in range(3):
-        hourglass_bottom = arr[(y * 6) + x]
+        hourglass_bottom = arr[(y * width) + x]
         x += 1
         hourglass.append(hourglass_bottom)
     return hourglass
 
 
-def sum_hourglasses():
-    pass
+def sum_hourglasses(arr):
+
+    width = find_width(arr)
+    index = 0
+    sum_hourglasses = 0
+    for number in arr:
+        coordinates = find_coordinates(index)
+        index += 1
+        if coordinates[1] < (width - 2):
+            if coordinates[0] < 4:
+                next_sum = sum(find_hourglass(coordinates))
+                if next_sum > sum_hourglasses:
+                    sum_hourglasses = next_sum
+        else:
+            continue
+    return sum_hourglasses
 
 
 # TESTS
@@ -67,4 +80,4 @@ def test_hourglass():
 
 
 def test_sum_hourglasses():
-    assert sum_hourglasses() == 25
+    assert sum_hourglasses(arr) == 28
