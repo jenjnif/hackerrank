bfc7c92188fc24d833094c26da5e24b3b2edfae3
jenjnif <jenjnif@hotmail.com>
2019-01-24 11:28:39 +0000

    pass first test and update test name

--- prime_numbers/finding_primes.py
+++ prime_numbers/finding_primes.py
@@ -1,11 +1,12 @@
 
 def find_primes(n):
     primes = []
+    p = 2
     for i in range(2, n):
         primes.append(i)
         print(primes)
     return primes
 
 
-def test_list_primes():
+def test_list_first_prime():
     assert find_primes(3) == [2]
