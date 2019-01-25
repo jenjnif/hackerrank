70ae147c71ac7dc2bd7b7cd99e1d85301c6061ff
jenjnif <jenjnif@hotmail.com>
2019-01-25 12:43:00 +0000

    pass test to find all primes up to n

--- prime_numbers/finding_primes.py
+++ prime_numbers/finding_primes.py
@@ -1,21 +1,33 @@
 
 def find_primes(n):
-    primes = [2]
-    p = 2
-    for i in range(2, n):
-        if i % p != 0:
+    primes = []
+    for i in range(2, n + 1):
+        if is_prime(i):
             primes.append(i)
+        print(i)
     print(primes)
     return primes
 
 
+def is_prime(x):
+    prime = True
+    for i in range(2, x):
+        if x % i == 0:
+            prime = False
+    return prime
+
+
 def test_list_first_prime():
-    assert find_primes(3) == [2]
+    assert find_primes(2) == [2]
 
 
 def test_list_two_primes():
-    assert find_primes(5) == [2, 3]
+    assert find_primes(3) == [2, 3]
 
 
-def test_list_all_primes_n():
+def test_list_all_primes_1():
     assert find_primes(10) == [2, 3, 5, 7]
+
+
+def test_list_all_primes_2():
+    assert find_primes(20) == [2, 3, 5, 7, 11, 13, 17, 19]
