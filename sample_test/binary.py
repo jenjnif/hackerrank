4982e8221a85ce49e8ed109e53ece4dd43105dfe
jenjnif <jenjnif@hotmail.com>
2019-01-31 17:10:02 +0000

    clear convert n decimal to binary

--- sample_test/binary.py
+++ sample_test/binary.py
@@ -4,29 +4,10 @@
 # e.g. 6 is 110 in binary with 2 consecutive 1's
 
 
-def list_binary(n):
-    binary = []
-    number = 1
-    while number < n:
-        binary.append(number)
-        number = number * 2
-    return binary[::-1]
-
-
-def decimal_binary(n):
-    binary_number = []
-    for i in list_binary(n):
-        if n - i >= 0:
-            print('n = ', n)
-            print('i = ', i)
-            binary_number.append(1)
-            n -= i
-        else:
-            binary_number.append(0)
-            print('correct?')
-    return binary_number
+def convert_decimal_binary(n):
+    pass
 
 
 # 10 is 1010
 #       8020
-print(decimal_binary(10))
+print(convert_decimal_binary(11))
