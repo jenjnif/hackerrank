c8dafad2d0ed18f1037e9c04b10c727b09587744
jenjnif <jenjnif@hotmail.com>
2019-02-01 09:22:08 +0000

    return consecutive ones in binary

--- sample_test/binary.py
+++ sample_test/binary.py
@@ -4,13 +4,28 @@
 # e.g. 6 is 110 in binary with 2 consecutive 1's
 
 
-def convert_decimal_binary(n):
+def decimal_to_binary_consecutive_ones(n):
     bit = []
+    counter = 0
+    consecutive = [0]
     while n > 0:
         bit.append(n % 2)
         n = n // 2
-    return bit[::-1]
+    binary = bit[::-1]
+    for i in range(len(binary)):
+        if binary[i] == 1:
+            counter += 1
+        else:
+            consecutive.append(counter)
+            counter = 0
+    if counter > 0:
+        consecutive.append(counter)
+    return max(consecutive)
 
 
 # 34 is 100010
-print(convert_decimal_binary(34))
+# 6 is 110
+# 355 is 101100011
+# 5 is 11
+# 367 is 101101111
+print(decimal_to_binary_consecutive_ones(367))
